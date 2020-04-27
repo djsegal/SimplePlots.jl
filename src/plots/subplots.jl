a0248ea6c7f3281abe8b1e55f732db387ac5f1ae
Dan Segal <dansegal2@gmail.com>
2020-04-27 03:09:19 -0700

    Get figsize working on composite plots

--- src/plots/subplots.jl
+++ src/plots/subplots.jl
@@ -1,9 +1,9 @@
-function plot(this_plot::SimplePlot, that_plot::SimplePlot, varargs...)
+function plot(this_plot::SimplePlot, that_plot::SimplePlot, varargs...; kwargs...)
   cur_plots = [
     this_plot, that_plot, varargs...
   ]
 
-  composite_plot = CompositePlot(cur_plots)
+  composite_plot = CompositePlot(cur_plots; kwargs...)
 
   composite_plot
 end
