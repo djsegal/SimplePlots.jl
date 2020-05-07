4c8497e98c2b8cf2a8a9ce7d4c0647730e07dccc
Dan Segal <dansegal2@gmail.com>
2020-05-07 16:43:37 -0700

    Refactor render to allow external calls on rendering html scripts

--- src/render.jl
+++ src/render.jl
@@ -9,28 +9,36 @@ end
 # render_unicode in repl.jl
 
 function render_html(cur_size, cur_data, cur_layout, cur_config)
-  cur_plot_id = string(UUIDs.uuid4())
-  cur_plot_div = "js-plot-" * string(cur_plot_id)
+  cur_plot_div = "js-plot-" * string(UUIDs.uuid4())
 
-  HTML(
-    """
-      <div id="$(cur_plot_div)" style="width:$(cur_size[1])px;height:$(cur_size[2])px;"></div>
+  cur_html = """
+    <div id="$(cur_plot_div)" style="width:$(cur_size[1])px;height:$(cur_size[2])px;"></div>
+  """
 
-      <script>
-        var anonFunc = function () {
-          plotDiv = document.getElementById('$(cur_plot_div)');
-          if ( plotDiv === null ) { return; }
-
-          Plotly.newPlot(
-            plotDiv,
-            $(JSON.json(cur_data)),
-            $(JSON.json(cur_layout)),
-            $(JSON.json(cur_config))
-          );
-        }
-
-        customPlotLoader(anonFunc);
-      </script>
-    """
+  cur_html *= _render_html_script(
+    cur_plot_div, cur_data, cur_layout, cur_config
   )
+
+  HTML(cur_html)
+end
+
+function _render_html_script(cur_plot_div, cur_data, cur_layout, cur_config)
+  """
+    <script class="js-new-plot-script">
+      var anonFunc = function () {
+        plotDiv = document.getElementById('$(cur_plot_div)');
+        if ( plotDiv === null ) { return; }
+        if ( plotDiv.childElementCount > 0 ) { return; }
+
+        Plotly.newPlot(
+          plotDiv,
+          $(JSON.json(cur_data)),
+          $(JSON.json(cur_layout)),
+          $(JSON.json(cur_config))
+        );
+      }
+
+      customPlotLoader(anonFunc);
+    </script>
+  """
 end
